6f1d6142bcd9cbbf5ca773eb832402d82e49662d
Marija Najdova <mnajdova@gmail.com>
2014-08-22 21:56:05 +0200

    Revert "Final Version"
    This reverts commit eb836a42d85dacd835127c09a5a26ab903414d58.

--- src/main/java/mk/ukim/finki/mp/stateful/payment/PaymentExecutor.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.mp.stateful.payment;
-
-public interface PaymentExecutor {
-
-	public boolean validatePaymentDetails(String name, String lastName,
-			String creditCardNumber);
-
-	public void executePayment(String creditCardNumber, double total);
-}
